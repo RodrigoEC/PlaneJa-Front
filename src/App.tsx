a4a8b38a00ac2781a4ce9aa82a8b57d11be49590
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-10-30 16:09:45 -0300

    Add styled components and base styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { dark, light } from './util/themes';
+import { GlobalStyle, Wrapper } from './globalStyles';
 
 function App() {
+  const [theme, setTheme] = useState(light)
+
+  const swapTheme = () => {
+    if (light === theme) setTheme(dark)
+    else setTheme(light)
+  }
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Wrapper onClick={swapTheme}>
+
+        </Wrapper>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/util/themes.ts
@@ -0,0 +1,13 @@
+export const light = {
+    primary: '#546D7D',
+    secondary: '#B7C5CE',
+    third: '#C2CFD7',
+    contrast: '#FFFFFF'
+}
+
+export const dark = {
+    primary: '#FFFFFF',
+    secondary: '#FFFFFF',
+    third: '#C2CFD7',
+    contrast: '#546D7D'
+}
