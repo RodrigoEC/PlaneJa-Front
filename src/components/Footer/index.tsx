f0a34d32afed17037a2069bec8459e43fe0c3844
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-11-04 08:57:54 -0300

    Refactoring footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,11 @@
 import { ReactElement } from "react";
-import {
-  Wrapper,
-  Text,
-  GithubIcon
-} from "./styles";
+import { Wrapper, Text, GithubIcon } from "./styles";
 
 export const Footer = (): ReactElement => {
-  return <Wrapper>
-    <Text>Contribue em:</Text>
-    <GithubIcon />
-  </Wrapper>;
+  return (
+    <Wrapper>
+      <Text>Contribua em:</Text>
+      <GithubIcon />
+    </Wrapper>
+  );
 };
