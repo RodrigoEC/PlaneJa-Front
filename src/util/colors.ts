dcc223ed8a9e8efd23f7424c979c1ae169f32764
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-11-04 08:48:32 -0300

    Adds colors file

---
+++ src/util/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  blue: "#7481C3",
+  cyan: "#74C3BF",
+  maroom: "#8A637D",
+  green: "#8EB26A",
+  red: "#C37474",
+  darkOrange: "#C39574",
+  lightOrange: "#DBB781",
+};
